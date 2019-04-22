11aa4c9034182bf789567d2e9e321ca56d3ac9d4
Tilwaen <tilwaenk@gmail.com>
2019-04-22 23:08:35 +0200

    Shards embed, fixed Infinity

--- commands/shards.js
+++ commands/shards.js
@@ -1,7 +1,7 @@
 const { Client } = require('unb-api');
+const { RichEmbed } = require('discord.js');
 
 exports.run = async (client, message, args, level, r) => {
-
     const colours = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"];
     const colourRoles = colours.map(colour => message.guild.roles.find(role => role.name === colour));
     var colourBalance = new Map();
@@ -56,43 +56,57 @@ exports.run = async (client, message, args, level, r) => {
 
     for (let [k, v] of colourBalance) {
         colourString = `${colourString}**${k}:** ${v}\n`;
-        colourTotal += v;
+        colourTotal = (v === "Infinity") ? "Infinity" : colourTotal + v;
     }
 
-    var text = `**Shards per colour:**\n\n${colourString}**Total colour net worth:** ${colourTotal}\n\n`;
-
+    var stringUsersGone = "";
+    var stringModerators = "";
+    var stringNotColoured = "";
     var usersGoneBalance = 0;
-    var usersModeratorsBalance = 0;
-    var usersNotColouredBalance = 0;
+    var moderatorsBalance = 0;
+    var notColouredBalance = 0;
 
     // Append additional text
     if (usersGone.length > 0) {
-        var stringUsersGone = `**User IDs of those who are not on the server anymore:**\n`;
-        usersGone.forEach(user => {
-            stringUsersGone = `${stringUsersGone}ID: ${user.userID}, balance: ${user.cash}\n`;
-            usersGoneBalance += user.cash;
-        });
-        text = text + stringUsersGone;
+        for (var user of usersGone) {
+            if (usersGoneBalance === "Infinity" || user.cash === "Infinity") {
+                usersGoneBalance = "Infinity";
+            }
+            stringUsersGone = `${stringUsersGone}${user.userID}: ${user.cash}\n`;
+        }
     }
     if (usersModerators.length > 0) {
-        var stringUsersModerators = `**Moderators who have shards:**\n`;
-        usersModerators.forEach(user => {
-            stringUsersModerators = `${stringUsersModerators}${user.user}, balance: ${user.cash}\n`;
-            usersModeratorsBalance += user.cash;
-        });
-        text = text + stringUsersModerators;
+        for (var user of usersModerators) {
+            if (moderatorsBalance === "Infinity" || user.cash === "Infinity") {
+                moderatorsBalance = "Infinity";
+            }
+            stringModerators = `${stringModerators}${user.user}: ${user.cash}\n`;
+        }
     }
     if (usersNotColoured.length > 0) {
-        var stringUsersNotColoured = `**Users who have shards and not a colour:**\n`;
-        usersNotColoured.forEach(user => {
-            stringUsersNotColoured = `${stringUsersNotColoured}${user.user}, balance: ${user.cash}\n`;
-            usersNotColouredBalance += user.cash;
-        });
-        text = text + stringUsersNotColoured;
+        for (var user of usersNotColoured) {
+            if (notColouredBalance === "Infinity" || user.cash === "Infinity") {
+                notColouredBalance = "Infinity";
+            }
+            stringNotColoured = `${stringNotColoured}${user.user}: ${user.cash}\n`;
+        }
     }
-    text = text + "**Total net worth:** " + (colourTotal + usersGoneBalance + usersModeratorsBalance + usersNotColouredBalance);
+    const balance = [colourTotal, usersGoneBalance, moderatorsBalance, notColouredBalance];
+    const total = balance.includes("Infinity") ? "Infinity" : balance.reduce((a, v) => a + v);
 
-    await msg.edit(text);
+    sendEmbed(msg, colourString, colourTotal, total, stringUsersGone, stringModerators, stringNotColoured);
+};
+
+async function sendEmbed(msg, colourString, colourTotal, total, usersGone, mods, uncoloured) {
+    var embed = new RichEmbed()
+        .setColor(0xC9DDFF)
+        .addField("Shard count", colourString)
+        .addField("Colour net worth:", colourTotal)
+        .addField("Total net worth:", total)
+        .addField("Users that are gone:", usersGone)
+        .addField("Moderators:", mods, true)
+        .addField("Uncoloured users:", uncoloured, true);
+    await msg.edit({ embed });
 };
 
 exports.conf = {
