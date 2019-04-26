de1581149f3c15012cabc0e0eb0c6b3be41f2d44
Tilwaen <tilwaenk@gmail.com>
2019-04-27 00:46:50 +0200

    typo in a message

--- commands/auth.js
+++ commands/auth.js
@@ -1,7 +1,7 @@
 exports.run = async (client, message, args, level, r, unbClient) => {
     const usernameBase64 = Buffer.from(message.author.tag).toString('base64');
     message.author.send(`Please authenticate your Reddit account through the link below:\n\nhttps://www.reddit.com/api/v1/authorize?client_id=${client.config.redditAuth.clientID}&response_type=code&state=${usernameBase64}&redirect_uri=${client.config.defaultSettings.oauthRedirectUri}&duration=temporary&scope=identity`);
-    message.reply(", check your DMs!");
+    message.reply("check your DMs!");
 };
 
 exports.conf = {
