4dcd0839aa6ed094b10f1ce3c26f5f847b10c5f1
Tilwaen <tilwaenk@gmail.com>
2019-04-28 16:40:41 +0200

    Archive poll

---
+++ commands/archivepoll.js
@@ -0,0 +1,46 @@
+const { RichEmbed } = require('discord.js');
+
+exports.run = async (client, message, args, level, r, unbClient) => {
+    if (args.length === 0) {
+        message.channel.send('You forgot to include the category name. The correct syntax is `archivepoll <channel category name>`.');
+        return;
+    }
+
+    const categoryName = args.join(' ');
+    const category = message.guild.channels
+            .filter(channel => channel.parent)  // Filter out channels that have a category
+            .map(channel => channel.parent)
+            .find(category => category.name.toLowerCase().includes(categoryName.toLowerCase()));
+
+    if (!category) {
+        message.channel.send(`Incorrect category name: \`${categoryName}\``);
+        return;
+    }
+
+    await message.channel.send(`--------------------\n**${category.name}**\n--------------------`);
+
+    for (var [channelID, channel] of category.children) {
+        const msg = await message.channel.send(`Archive channel ${channel}?`);
+        await msg.react("👍");
+        await msg.react("👎");
+        await msg.react("🤷");
+    }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["pollarchive"],
+  permLevel: "Oil",
+  channelPerms: "All",
+  userCooldown: false,
+  globalCooldown: false,
+  cooldownDuration: 0
+};
+
+exports.help = {
+  name: "archivepoll",
+  category: "Miscelaneous",
+  description: "Create simple polls (upvote/downvote/shrug) to archive each channel of the given category.",
+  usage: "archivepoll <channel category name>"
+};
