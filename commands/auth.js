963342ef5d51f316536c7d23f64d0a25e7623269
Tilwaen <tilwaenk@gmail.com>
2019-04-30 12:52:56 +0200

    Changed the Auth DM message to reassure the user that no sensitive info is gained

--- commands/auth.js
+++ commands/auth.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args, level, r, unbClient) => {
     }
 
     const usernameBase64 = Buffer.from(message.author.id).toString('base64');
-    message.author.send(`Please authenticate your Reddit account through the link below:\n\nhttps://www.reddit.com/api/v1/authorize?client_id=${client.config.redditAuth.clientID}&response_type=code&state=${usernameBase64}&redirect_uri=${client.config.oauth.redirectUri}&duration=temporary&scope=identity`);
+    message.author.send(`Please authenticate your Reddit account through the link below. **We do not gain any sensitive data about your account**, all we do is verify that you're not claiming to be someone else.\n\nhttps://www.reddit.com/api/v1/authorize?client_id=${client.config.redditAuth.clientID}&response_type=code&state=${usernameBase64}&redirect_uri=${client.config.oauth.redirectUri}&duration=temporary&scope=identity`);
     message.reply("check your DMs!");
 };
 
