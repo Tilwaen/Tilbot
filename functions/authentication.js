a1041bca1e5521fc9a3393093fc5e5a881b9c445
Tilwaen <tilwaenk@gmail.com>
2019-04-30 12:35:25 +0200

    Better await all the shit

--- functions/authentication.js
+++ functions/authentication.js
@@ -8,7 +8,7 @@ module.exports = {
 
         if (!message) {
             const user = await client.fetchUser(userID);
-            user.send("You took too long to authenticate. Try to do the command again or contact any human (mini)mod to get your role assigned.");
+            await user.send("You took too long to authenticate. Try to do the command again or contact any human (mini)mod to get your role assigned.");
             return;
         }
 
@@ -24,7 +24,7 @@ module.exports = {
         try {
             userFlair = await r.getSubreddit('flairwars').getUserFlair(redditUsername);
         } catch (error) {
-            message.channel.send("This is not a valid Reddit account: https://www.reddit.com/u/" + redditUsername);
+            await message.channel.send("This is not a valid Reddit account: https://www.reddit.com/u/" + redditUsername);
             return;
         };
         // Split at space and take the first part because of the totem season ticks (filter only the colour)
@@ -40,12 +40,12 @@ module.exports = {
         // If they don't have any flair, let them assign a flair first
         const megathreadLink = 'https://www.reddit.com/r/flairwars/comments/bavhvo/comment_to_gain_a_team_megathread/';
         if (flair === 'None') {
-            message.reply(`you don't have any flair assigned on the subreddit yet. Please leave a comment under the Megathread to get your colour assigned. You cannot choose the colour, the flair distribution process is automatic and random. May you get assigned to the best team!\n\n${megathreadLink}`);
+            await message.reply(`you don't have any flair assigned on the subreddit yet. Please leave a comment under the Megathread to get your colour assigned. You cannot choose the colour, the flair distribution process is automatic and random. May you get assigned to the best team!\n\n${megathreadLink}`);
             return;
         }
 
         if (flair === 'Mod') {
-            message.reply(`I can't assign a moderator role, so I'll just dab on you instead`);
+            await message.reply(`I can't assign a moderator role, so I'll just dab on you instead`);
             return;
         }
 
@@ -55,7 +55,6 @@ module.exports = {
 
         const now = new Date().getTime() / 1000;  // ms
         const ageInDays = (now - accountCreated)/60/60/24; // s/min/hours
-        console.log(ageInDays);
 
         // The karma and age are not sufficient
         if (karma < karmaLimit || ageInDays < ageLimit) {
@@ -63,7 +62,7 @@ module.exports = {
             // Send an embed to the need-role channel
             const needRoleChannel = message.guild.channels.get(client.config.oauth.needRolesChannelID);
             if (!needRoleChannel) {
-                message.channel.send(`Error: Could not find the channel with ID ${client.config.oauth.needRolesChannelID} to log this event and notify the minimods that this user needs their attention; please tell the bot admins to solve this mess.`);
+                await message.channel.send(`Error: Could not find the channel with ID ${client.config.oauth.needRolesChannelID} to log this event and notify the minimods that this user needs their attention; please tell the bot admins to solve this mess.`);
                 return;
             }
             await sendRedditUserEmbed(needRoleChannel, discordUser, redditUsername, flair, karma, age);
@@ -82,19 +81,19 @@ module.exports = {
 
         const colourRole = message.guild.roles.find(role => role.name === flair);
         if (!colourRole) {
-            message.channel.send(`Error: Didn't find a role with name \`${flair}\``);
+            await message.channel.send(`Error: Didn't find a role with name \`${flair}\``);
             return;
         }
 
         // Assign the colour and take the No Role
         const noRoleRole = message.guild.roles.find(role => role.name === client.config.defaultSettings.noRole);
-        guildMember.addRole(colourRole);
-        guildMember.removeRole(noRoleRole);
+        await guildMember.addRole(colourRole);
+        await guildMember.removeRole(noRoleRole);
 
         // Send the embed to the logging channel that the user was let in automatically
         const loggingChannel = message.guild.channels.get(client.config.oauth.botAuthLoggingChannelID);
         if (!loggingChannel) {
-            message.channel.send(`Error: Could not find the channel with ID ${client.config.oauth.botAuthLoggingChannelID} to log that this user has been let into the server automatically; please tell the bot admins to solve this mess.`);
+            await message.channel.send(`Error: Could not find the channel with ID ${client.config.oauth.botAuthLoggingChannelID} to log that this user has been let into the server automatically; please tell the bot admins to solve this mess.`);
             return;
         }
         await sendRedditUserEmbed(loggingChannel, discordUser, redditUsername, flair, karma, age, `User was let in automatically`, `${discordUser}\n[${redditUsername}](https://www.reddit.com/u/${redditUsername})`);
@@ -103,13 +102,13 @@ module.exports = {
         // Get the colour general channel
         const colourGeneralChannel = message.guild.channels.find(channel => channel.name === `${flair.toLowerCase()}-general`);
         if (!colourGeneralChannel) {
-            loggingChannel.send(`Error: Could not find the channel with name ${flair.toLowerCase()}-general welcome this user to their colour.`);
+            await loggingChannel.send(`Error: Could not find the channel with name ${flair.toLowerCase()}-general welcome this user to their colour.`);
             return;
         }
         // Get the welcome message
         var welcomeMessage = client.config.oauth.welcomeMessage;
         if (!welcomeMessage) {
-            loggingChannel.send(`Error: Could not load the welcome message to welcome user in ${colourGeneralChannel}.`);
+            await loggingChannel.send(`Error: Could not load the welcome message to welcome user in ${colourGeneralChannel}.`);
             return;
         }
         welcomeMessage = welcomeMessage.replace("{{user}}", discordUser);
@@ -120,7 +119,7 @@ module.exports = {
             welcomeMessage = welcomeMessage + ` and don't forget to join the ${flair} server: ${colourServerInviteLink}!`;
         }
         // Send the welcome message
-        colourGeneralChannel.send(welcomeMessage);
+        await colourGeneralChannel.send(welcomeMessage);
     },
     authFailure: async function (client, r, authReq, response, state) {
         const userID = Buffer.from(state, 'base64').toString('ascii');
