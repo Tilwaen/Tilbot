902cff50df01165ef942899adfe8904341706dca
Tilwaen <tilwaenk@gmail.com>
2019-06-03 15:44:51 +0200

    The initiator of the purge must confirm it within 2 minutes after being pinged

--- commands/userpurge.js
+++ commands/userpurge.js
@@ -42,26 +42,38 @@ exports.run = async (client, message, args, level, r, unbClient) => {
     }
 
     console.log(`Total number of users: ${message.guild.memberCount}.\nNumber of users last active before ${inputDate} to be purged: ${usersNotFound.size}.\nNumber of users that will be left: ${usersFound.size}.`);
-    const msg2 = await message.channel.send(`Total number of users: **${message.guild.memberCount}**.\nNumber of users last active before *${inputDate}* to be purged: **${usersNotFound.size}**.\nNumber of users that will be left: **${usersFound.size}**.`);
-    const msg3 = await message.channel.send(`**Users to be purged:**`);
+    await message.channel.send(`Total number of users: **${message.guild.memberCount}**.\nNumber of users last active before *${inputDate}* to be purged: **${usersNotFound.size}**.\nNumber of users that will be left: **${usersFound.size}**.`);
+    await message.channel.send(`**Users to be purged:**`);
     await sendTrimmedMessage(message.channel, stringUsersNotFound);
     if (usersNotFoundButNotKickable.size > 0) {
-        const msg4 = await message.channel.send(`**Users that aren't active but can't be kicked by this bot:**\n${stringUsersNotFoundButNotKickable}`);
+        await message.channel.send(`**Users that aren't active but can't be kicked by this bot:**\n${stringUsersNotFoundButNotKickable}`);
     }
 
     // Kick the users if running in a live mode
     if (runInLiveMode) {
-        console.log("Running in live mode, kicking users.");
-        const msg5 = await message.channel.send(`Running in live mode, kicking users. Bots will not be kicked.`);
-        await usersNotFound.forEach(function(member) {
-            if (!member.kickable) return message.reply(`I cannot kick member ${member}`);
-            if (member.user.bot) return message.reply(`${member} is a bot, I will not kick it`);
-            console.log(`Kicking ${member}`);
-            member.kick("Purge of inactive users");
-        });
+        await message.reply("kick users? Confirm with 'y':");
+
+        // The author of the message needs to confirm the selection
+        const filter = response => {
+            return response.author === message.author && response.content.toLowerCase() === 'y';
+        }
+
+        try {
+            const collected = await message.channel.awaitMessages(filter, { maxMatches: 1, time: 120000, errors: ['time'] });
+            await message.channel.send(`Kicking users. Bots will not be kicked.`);
+            await usersNotFound.forEach(member => {
+                if (!member.kickable) return message.reply(`I cannot kick member ${member}`);
+                if (member.user.bot) return message.reply(`${member} is a bot, I will not kick it`);
+                console.log(`Kicking ${member}`);
+                member.kick("Purge of inactive users");
+            });
+        } catch (time) {
+            await message.channel.send('Time out, no users will be kicked.');
+            return;
+        }
     } else {
         console.log("Running in test mode, no users were kicked.");
-        const msg5 = await message.channel.send(`Running in test mode, no users were kicked.`);
+        await message.channel.send(`Running in test mode, no users were kicked.`);
     }
 };
 
