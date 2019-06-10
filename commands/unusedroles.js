3284f825fb4ad6ba80c6f1b8fd55932ad77eb2ac
Tilwaen <tilwaenk@gmail.com>
2019-06-10 16:40:56 +0200

    IDs as strings...

--- commands/unusedroles.js
+++ commands/unusedroles.js
@@ -3,8 +3,8 @@ exports.run = async (client, message, args, level, r, unbClient) => {
     let unusedRoles = message.guild.roles                                                                           // Filter out roles to contain...
                                     .filter(role => !message.guild.members                                          // no such role that...
                                                         .some(member => member.roles.has(role.id)                   // any user would have this role...
-                                                                        && (member.user.id !== 391963807771000843   // unless the user is (SSeptic || boopis)
-                                                                            && member.user.id !== 476536649074081802)));
+                                                                        && (member.user.id !== '391963807771000843'   // unless the user is (SSeptic || boopis)
+                                                                            && member.user.id !== '476536649074081802')));
     await message.reply(`Number of unused roles: ${unusedRoles.size}\n${unusedRoles.map(role => role.name).join('\n')}`);
 };
 
