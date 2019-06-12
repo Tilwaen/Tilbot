de97af610a60f4a5b9b271c4ed5f169b82fb9983
Tilwaen <tilwaenk@gmail.com>
2019-06-12 21:13:09 +0200

    Servericon command

---
+++ commands/servericon.js
@@ -0,0 +1,23 @@
+const imageReply = require('../functions/imagereply.js');
+
+exports.run = async (client, message, args, level, r, unbClient) => {
+    await imageReply.sendImageEmbed(message.channel, message.guild.iconURL);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["serveravatar", "serverpfp"],
+  permLevel: "User",
+  channelPerms: "All",
+  userCooldown: false,
+  globalCooldown: false,
+  cooldownDuration: 0
+};
+
+exports.help = {
+  name: "servericon",
+  category: "Miscelaneous",
+  description: "Posts the server icon",
+  usage: "servericon"
+};
